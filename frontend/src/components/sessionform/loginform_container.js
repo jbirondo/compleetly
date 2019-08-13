9718ba84016a94ecf36128ec1f3df5f6f8c5fbc2
Camlgiles <cgiles@gilescpa.com>
2019-08-13 13:20:47 -0700

    add sessionForm

---
+++ frontend/src/components/sessionform/loginform_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+import LoginForm from './login_form';
+
+const msp = state => ({
+    // errors: state.errors.session
+})
+
+const mdp = dispatch => ({
+    login: user => dispatch(login(user))
+})
+
+export default connect(msp, mdp)(LoginForm);
