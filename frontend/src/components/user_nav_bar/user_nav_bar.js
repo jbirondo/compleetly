7017b1d0baf182f4aec3658bcb3ed6a105e61f08
Ken Klooster <kklooster3910@gmail.com>
2019-08-14 22:58:53 -0400

    commit working navbar with links to news source

--- frontend/src/components/user_nav_bar/user_nav_bar.js
+++ frontend/src/components/user_nav_bar/user_nav_bar.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { receiveCurrentUser } from '../../actions/session_actions';
 // import Follow from "../../../../models/Follow"
 class UserNavBar extends React.Component {
     constructor(props) {
         super(props);
 
     }
+
+    componentDidMount() {
+        // debugger;
+        this.props.receiveCurrentUser(this.props.currentUser);
+    }
+
     renderFollows() {
         // debugger
+
         return (
             <ul>    
-                {this.props.currentUser.followedSources.map(
-                    source =>   
-                    <li key={source}>{source}</li>
-                )}
+                {this.props.follows.map(source =>   
+                    <li key={source._id}><a href={source.followURL}>{source.followName}</a></li>
+            )}
             </ul>
         )
     }
 
     render() {
+        if (!this.props.follows) {
+            return null;
+        }
         return (
             <div className="user-bar-container" >
                 <Link to={`/${this.props.currentUserId}/today`}>Today</Link>
@@ -28,11 +38,6 @@ class UserNavBar extends React.Component {
                 <div>Feeds <Link to={`/${this.props.currentUserId}/organize`}>⚙︎</Link></div>
                 {this.renderFollows()}
                   
-
-
-
-
-
                 <Link to={`/newsfeed/add`}>+ ADD CONTENT</Link>
             </div>
         );
@@ -40,19 +45,24 @@ class UserNavBar extends React.Component {
 }
 
 
-
-
 const mstp = state => {
-    // debugger
+    const user = state.session.user;
+    let follows;
+    // debugger;
+    if (!user.sourcesArray) {
+        follows = user.followedSources.map(id => state.entities.follows[id] )
+    }
+
     return {
         isAuthenticated: state.session.isAuthenticated,
         currentUserId: state.session.user.id,
-        currentUser: state.session.user
+        currentUser: state.session.user,
+        follows: follows
     }
 }
 
-const mdtp = dispatch => {
-
-}
+const mdtp = dispatch => ({
+    receiveCurrentUser: user => dispatch(receiveCurrentUser(user)), 
+})
 
 export default connect(mstp, mdtp)(UserNavBar);
