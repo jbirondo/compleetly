7d119fead5378811fde4b0032c11e65e581d5e17
Camlgiles <cgiles@gilescpa.com>
2019-08-16 11:00:10 -0700

    add unfollow btn to nav_bar feed

--- frontend/src/components/user_nav_bar/user_nav_bar.js
+++ frontend/src/components/user_nav_bar/user_nav_bar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom'
 import { fetchUser } from '../../actions/session_actions';
+import {deleteFollow} from '../../actions/follow_actions';
 // import SourceArticlesShow from './source_articles_show'
 // import Follow from "../../../../models/Follow"
 class UserNavBar extends React.Component {
     constructor(props) {
         super(props);
+        this.articles = null;
     }
 
     componentDidMount() {
@@ -14,17 +16,39 @@ class UserNavBar extends React.Component {
     }
 
     renderFollows() {
+        // debugger
+        this.articles = Object.values(this.props.articles);
+        let followName = []; // ['bloomberg', 'nbc', 'cnbc']
+        let follows = [];
 
+        if (this.articles.length > 0 && this.articles) {
+            this.articles = this.articles.map((article, i) => {
+                // debugger;
+                Object.values(this.props.follows).forEach(follow => followName.push(follow.followName));
+
+                Object.values(this.props.follows).forEach(follow => follows.push(follow));
+
+                follows.forEach(follow => {
+                    if (follow.followName.includes(article.name)) {
+                        article.followId = follow._id;
+                    }
+                })
+            });
+        };
             // <Link to={`/sources/articles`}><li key={source._id}>{source.followName}</li></Link>
                     // <button onClick={() => this.props.history.push(`/sources/articles`)}>{source.followName}</button>  
         return (
             <ul>    
-                {this.props.follows.map(source =>
-                <li key={source._id}>
-                    {/* <SourceArticlesShow source={source} /> */}
-                    <Link to={ { pathname: `/${source.followName}/articles`, state: {source: source} }}>{source.followName}</Link>
-                </li>
-                )}
+                {this.props.follows.map(source => {
+                    // debugger
+                    return (
+                        <li key={source._id}>
+                            {/* <SourceArticlesShow source={source} /> */}
+                            <Link to={ { pathname: `/${source.followName}/articles`, state: {source: source} }}>{source.followName}</Link>
+                                <button onClick={() => this.props.deleteFollow({ followId: source._id,   currentUserId: this.props.currentUserId })}>Unfollow</button>
+                                {/* followId: article.followId, */}
+                        </li>
+                )})} 
             </ul>
         )
     }
@@ -60,6 +84,7 @@ class UserNavBar extends React.Component {
 
 
 const mstp = state => {
+    // debugger
     const user = state.session.user;
     let follows;
     // debugger;
@@ -74,6 +99,7 @@ const mstp = state => {
 
 
     return {
+        articles: state.entities.articles,
         isAuthenticated: state.session.isAuthenticated,
         currentUserId: state.session.user.id,
         currentUser: state.session.user,
@@ -83,7 +109,8 @@ const mstp = state => {
 }
 
 const mdtp = dispatch => ({
-    fetchUser: user => dispatch(fetchUser(user)), 
+    fetchUser: user => dispatch(fetchUser(user)),
+    deleteFollow: follow => dispatch(deleteFollow(follow)) 
 })
 
 export default withRouter(connect(mstp, mdtp)(UserNavBar));
