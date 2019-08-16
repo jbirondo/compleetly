2e773a99b224b83e76fad8389675caad3c46aa06
Camlgiles <cgiles@gilescpa.com>
2019-08-16 08:24:06 -0700

    add unfollow to all content pages

--- frontend/src/components/newsfeed_addcontent/content_pages/entertainment.js
+++ frontend/src/components/newsfeed_addcontent/content_pages/entertainment.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { createFollow } from '../../../actions/follow_actions';
+import { createFollow, deleteFollow } from '../../../actions/follow_actions';
 // const NewsAPI = require('newsapi');
 // const newsapi = new NewsAPI('c74b69f1594f4080902981643aa178df');
 
@@ -32,10 +32,33 @@ class EntertainmentFeed extends React.Component {
 
 
     render() {
+        // debugger
         let articles;
-        articles = this.state.articles.map((article, i) => <li key={i}>{article.name} {article.url}
-            <button onClick={() => this.props.createFollow({ followName: article.name, followURL: article.url, currentUserId: this.props.currentUserId })}>Follow ME!</button>
-        </li>)
+        articles = this.state.articles.map((article, i) => {
+            // debugger;
+            let followName = []; // ['bloomberg', 'nbc', 'cnbc']
+            Object.values(this.props.entities.follows).forEach(follow => followName.push(follow.followName));
+
+            let follows = [];
+            Object.values(this.props.entities.follows).forEach(follow => follows.push(follow));
+
+            follows.forEach(follow => {
+                if (follow.followName.includes(article.name)) {
+                    article.followId = follow._id;
+                }
+            })
+
+            // debugger;
+            if (!!followName.includes(article.name)) {
+                return (<li key={i}>{article.name} {article.url}
+                    <button onClick={() => this.props.deleteFollow({ followId: article.followId, currentUserId: this.props.currentUserId })}>UNFOLLOW ME</button>
+                </li>)
+            } else {
+                return (<li key={i}>{article.name} {article.url}
+                    <button onClick={() => this.props.createFollow({ followName: article.name, followURL: article.url, currentUserId: this.props.currentUserId })}>Follow ME!</button>
+                </li>)
+            }
+        })
 
         return (
             <div>
@@ -48,11 +71,14 @@ class EntertainmentFeed extends React.Component {
 
 const msp = state => ({
     // errors: state.errors.follows // don't have error reducers set up
-    currentUserId: state.session.user.id
+    currentUserId: state.session.user.id,
+    currentUser: state.session.user,
+    entities: state.entities
 })
 
 const mdp = dispatch => ({
     createFollow: follow => dispatch(createFollow(follow)),
+    deleteFollow: follow => dispatch(deleteFollow(follow))
 })
 
 export default withRouter(connect(msp, mdp)(EntertainmentFeed));
