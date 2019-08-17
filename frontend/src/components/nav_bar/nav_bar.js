c1846107ffc17b85e2e5a392430f6ae662d448d8
Camlgiles <cgiles@gilescpa.com>
2019-08-16 17:10:44 -0700

    additional nav bar styling

--- frontend/src/components/nav_bar/nav_bar.js
+++ frontend/src/components/nav_bar/nav_bar.js
@@ -5,15 +5,15 @@ import './navbar.css';
 export default ({ isAuthenticated, currentUser, logout, openModal }) => {
     // debugger
     const display = isAuthenticated ? (
-        <div className='nav-bar-logged-in'>
+        <div className='upper-nav-bar'>
            {/* <h1 className='nav-bar-current-user'>{currentUser.firstName}</h1>
            <button onClick={() => logout()}>Log Out</button> */}
             <UserDropdown/>
         </div>
     ) : (
-         <div className='nav-bar-session-form'>
-             <button className="nav-bar-sign-up-btn" onClick={() => openModal('signup')}>Sign Up</button>
-             <button className="nav-bar-log-in-btn" onClick={() => openModal('login')}>Log In</button>
+         <div className='upper-nav-bar sign-in-nav-bar'>
+             <button className="nav-bar-sign-up-btn nav-bar-btn" onClick={() => openModal('signup')}>Sign Up</button>
+             <button className="nav-bar-log-in-btn nav-bar-btn" onClick={() => openModal('login')}>Log In</button>
          </div>
     )
 
