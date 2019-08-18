59d132897765b9c0333021b05b9834b468d55154
Eugene Birondo <jbirondo@gmail.com>
2019-08-17 22:18:28 -0700

    Added more grids

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -3,12 +3,12 @@ import { openModal } from '../../actions/modal_actions'
 import {connect} from 'react-redux';
 import './mainPage.css'
 import landing from "./landing.png"
-import nyticon from './new-york-times-icon.png'
-import tcicon from './techcrunch-icon.png'
-import yticon from './youtube-icon.png'
-import gicon from './google-icon.png'
-import ticon from './twitter-icon.png'
-import rssicon from './rss-feed-icon.png'
+import nyticon from './icons/new-york-times-icon.png'
+import tcicon from './icons/techcrunch-icon.png'
+import yticon from './icons/youtube-icon.png'
+import gicon from './icons/google-icon.png'
+import ticon from './icons/twitter-icon.png'
+import rssicon from './icons/rss-feed-icon.png'
 
 class MainPage extends React.Component {
     constructor(props){
@@ -109,7 +109,158 @@ class MainPage extends React.Component {
                 </div>
               </div>
             </div>
-
+            <div className="main-page-section-features-container">
+              <div className="main-page-section-kicker">
+                The best of the web
+              </div>
+              <h2 className="main-page-section-header">
+                Discover insightful sources
+              </h2>
+              <div className="main-page-section-features-grid-container">
+                <div className="main-page-section-feature-grid-item">
+                  <img src={nyticon} className="main-page-section-feature-grid-item-picture-1" />
+                  <div className="main-page-section-feature-grid-item-content-1">
+                    <div className="grid-item-header-1">
+                      Publications
+                    </div>
+                    <div className="grid-item-description-1">
+                      Don't miss a beat from New York Times or any other industry journal you trust
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={tcicon} className="main-page-section-feature-grid-item-picture-2" />
+                  <div className="main-page-section-feature-grid-item-content-2">
+                    <div className="grid-item-header-2">
+                      Blogs
+                    </div>
+                    <div className="grid-item-description-2">
+                      Dive deeper by following blogs from the latest movers, shakers, and thinkers
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={yticon} className="main-page-section-feature-grid-item-picture-3" />
+                  <div className="main-page-section-feature-grid-item-content-3">
+                    <div className="grid-item-header-3">
+                      YouTube Channels
+                    </div>
+                    <div className="grid-item-description-3">
+                      See new videos from the YouTube channels you follow
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={gicon} className="main-page-section-feature-grid-item-picture-4" />
+                  <div className="main-page-section-feature-grid-item-content-4">
+                    <div className="grid-item-header-4">
+                      Keyword Alerts
+                    </div>
+                    <div className="grid-item-description-4">
+                      Monitor news about your company, your product, your craft, and your competitors
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={ticon} className="main-page-section-feature-grid-item-picture-5" />
+                  <div className="main-page-section-feature-grid-item-content-5">
+                    <div className="grid-item-header-5">
+                      Tweets
+                    </div>
+                    <div className="grid-item-description-5">
+                      Follow your favorite twitter influencer or hashtag and never miss crucial updates
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={rssicon} className="main-page-section-feature-grid-item-picture-6" />
+                  <div className="main-page-section-feature-grid-item-content-6">
+                    <div className="grid-item-header-6">
+                      RSS Feeds
+                    </div>
+                    <div className="grid-item-description-6">
+                      Follow anyone on the Web who publishes an RSS feed
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="main-page-section-features-container">
+              <div className="main-page-section-kicker">
+                The best of the web
+              </div>
+              <h2 className="main-page-section-header">
+                Discover insightful sources
+              </h2>
+              <div className="main-page-section-features-grid-container">
+                <div className="main-page-section-feature-grid-item">
+                  <img src={nyticon} className="main-page-section-feature-grid-item-picture-1" />
+                  <div className="main-page-section-feature-grid-item-content-1">
+                    <div className="grid-item-header-1">
+                      Publications
+                    </div>
+                    <div className="grid-item-description-1">
+                      Don't miss a beat from New York Times or any other industry journal you trust
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={tcicon} className="main-page-section-feature-grid-item-picture-2" />
+                  <div className="main-page-section-feature-grid-item-content-2">
+                    <div className="grid-item-header-2">
+                      Blogs
+                    </div>
+                    <div className="grid-item-description-2">
+                      Dive deeper by following blogs from the latest movers, shakers, and thinkers
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={yticon} className="main-page-section-feature-grid-item-picture-3" />
+                  <div className="main-page-section-feature-grid-item-content-3">
+                    <div className="grid-item-header-3">
+                      YouTube Channels
+                    </div>
+                    <div className="grid-item-description-3">
+                      See new videos from the YouTube channels you follow
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={gicon} className="main-page-section-feature-grid-item-picture-4" />
+                  <div className="main-page-section-feature-grid-item-content-4">
+                    <div className="grid-item-header-4">
+                      Keyword Alerts
+                    </div>
+                    <div className="grid-item-description-4">
+                      Monitor news about your company, your product, your craft, and your competitors
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={ticon} className="main-page-section-feature-grid-item-picture-5" />
+                  <div className="main-page-section-feature-grid-item-content-5">
+                    <div className="grid-item-header-5">
+                      Tweets
+                    </div>
+                    <div className="grid-item-description-5">
+                      Follow your favorite twitter influencer or hashtag and never miss crucial updates
+                    </div>
+                  </div>
+                </div>
+                <div className="main-page-section-feature-grid-item">
+                  <img src={rssicon} className="main-page-section-feature-grid-item-picture-6" />
+                  <div className="main-page-section-feature-grid-item-content-6">
+                    <div className="grid-item-header-6">
+                      RSS Feeds
+                    </div>
+                    <div className="grid-item-description-6">
+                      Follow anyone on the Web who publishes an RSS feed
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         );
     }
