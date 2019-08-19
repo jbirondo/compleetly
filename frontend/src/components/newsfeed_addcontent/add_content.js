1589f3f2d39a1388529fb9074a44eb63453cf86c
Ken Klooster <kklooster3910@gmail.com>
2019-08-19 14:12:03 -0400

    commit working on add content page css

--- frontend/src/components/newsfeed_addcontent/add_content.js
+++ frontend/src/components/newsfeed_addcontent/add_content.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
-
+import tech from './tech.png';
+import sports from './sports.png';
+import science from './science.png';
+import health from './health.png';
+import general from './general.png';
+import entertainment from './entertainment.png';
+import business from './business.png';
+import rss from '../main/icons/rss-feed-icon.png';
+import './add_content.css';
 
 // const NewsAPI = require('newsapi');
 // const newsapi = new NewsAPI('0fe3c7ee9aa4446d94b11b44f28c4b74');
@@ -15,15 +23,21 @@ class AddContent extends React.Component {
     render() {
 
         return (
-            <div>
-                {/* {articles} */}
-                <Link to='/newsfeed/technology'>CLICK ME FOR TECHNOLOGY ARTICLES</Link>
-                <Link to='/newsfeed/sports'>CLICK ME FOR SPORTS ARTICLES</Link>
-                <Link to='/newsfeed/science'>CLICK ME FOR SCIENCE ARTICLES</Link>
-                <Link to='/newsfeed/health'>CLICK ME FOR HEALTH ARTICLES</Link>
-                <Link to='/newsfeed/general'>CLICK ME FOR GENERAL ARTICLES</Link>
-                <Link to='/newsfeed/entertainment'>CLICK ME FOR ENTERTAINMENT ARTICLES</Link>
-                <Link to='/newsfeed/business'>CLICK ME FOR BUSINESS ARTICLES</Link>
+            <div className='add-content-page-container'>
+                <div className='rss-icon-container'>
+                    
+                    <div className="add-content-rss-sources-item" ><img alt=" " src={rss} className="add-content-rss-sources-item-image" /> Sources to Follow</div>
+                </div>
+                <div className='add-content-discover-header'>Discover the best sources for any topic</div>
+                <div className='add-content-categories-container'>
+                    <Link to='/newsfeed/technology'><img className='add-content-tech-image' alt=' ' src={tech} /><div className='category-title'>Tech></div></Link>
+                    <Link to='/newsfeed/sports'><img className='add-content-sports-image' alt=' ' src={sports} />Sports</Link>
+                    <Link to='/newsfeed/science'><img className='add-content-science-image' alt=' ' src={science} />Science</Link>
+                    <Link to='/newsfeed/health'><img className='add-content-health-image' alt=' ' src={health} />Health</Link>
+                    <Link to='/newsfeed/general'><img className='add-content-general-image' alt=' ' src={general} />General</Link>
+                    <Link to='/newsfeed/entertainment'><img className='add-content-entertainment-image' alt=' ' src={entertainment} />Entertainment</Link>
+                    <Link to='/newsfeed/business'><div className='category-title-container'><img className='add-content-business-image' alt=' ' src={business} /><div className='category-title'>Business</div></div></Link>
+                </div>
             </div>
         )
     }
