0f434ae5d2972bf5d03c405a6194f617e4e97275
Camlgiles <cgiles@gilescpa.com>
2019-08-19 08:48:28 -0700

    add heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
@@ -7,6 +8,15 @@ const passport = require('passport')
 
 const users = require("./routes/api/users");
 // const follows = require("./routes/api/follows?");
+
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('frontend/build'));
+   app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+   })
+}
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+   mongoURI: 'mongodb+srv://camlgiles:Y936FtwkMw6!$C@cluster0-rkvdz.mongodb.net/test?retryWrites=true&w=majority',
+   secretOrKey: '3ng72bsy4RU3OnV8tXzkuBAYi49e3atC'
+}
