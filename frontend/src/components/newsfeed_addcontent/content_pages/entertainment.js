e1c72bb180cad15d3c99a5046ed38f1c926012bf
Eugene Birondo <jbirondo@gmail.com>
2019-09-24 08:41:44 -0700

    Fixed add content spacing

--- frontend/src/components/newsfeed_addcontent/content_pages/entertainment.js
+++ frontend/src/components/newsfeed_addcontent/content_pages/entertainment.js
@@ -38,7 +38,7 @@ class EntertainmentFeed extends React.Component {
 
         return ( // article componenet that only receives articles as props, only render this component if I have these props. have render component do NO fetching logic
             // only pass articles to render component when I have successfully fetched
-            <div>
+            <div className="add-content-list-container">
                 {articles}
             </div>
         )
